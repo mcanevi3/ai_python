684df61419a90bb89a3c1d72225837047af894b0
Mehmet Canevi <m.canevi3@gmail.com>
2025-08-10 17:05:23 +0300

    some fixes

--- lyap_counterex1/test2.py
+++ lyap_counterex1/test2.py
@@ -7,7 +7,7 @@ import torch
 
 A=torch.tensor([[1.0,2.0],[-3.0,-4.0]])
 
-grid = torch.linspace(-10.0, -5, 3)
+grid = torch.linspace(-2.0, 2.0, 30)
 x1, x2= torch.meshgrid(grid, grid, indexing='ij')
 x_train = torch.stack([x1.flatten(), x2.flatten()], dim=1).T
 
@@ -16,26 +16,37 @@ V=(x_train.T@P@x_train).diagonal()
 xdot_train=A@x_train
 gradxdot_train=A
 Vdot=(xdot_train.T@P@x_train+x_train.T@P@xdot_train).diagonal()
-
+Vdot_res=torch.relu(Vdot)
+Vdot_count=Vdot_res.sum().item()
 
 lmi1=A.T@P+P@A
 eigVal,eigVec=torch.linalg.eig(lmi1)
 print(f"Eigs of A^TP+PA:{eigVal.tolist()}")
 
-gradVdot=2*(gradxdot_train.T@P@x_train+P@xdot_train)
-
-print("Vdot : [" + ", ".join(f"{v:10.3f}" for v in Vdot.tolist()) + "]")
-plt.figure()
-plt.plot(x_train[1,:],Vdot,'kx')
-
+print(f"Vdot:{Vdot_res.tolist()}")
+costvec=torch.zeros((30+1,))
+costvec[0]=Vdot_count
+costindex=torch.zeros_like(costvec)
 x_train2=x_train
-for i in range(3):
-    x_train2=x_train2+0.01*gradVdot*Vdot*+1
-
+for i in range(1,costvec.shape[0]):
     xdot_train2=A@x_train2
+    gradVdot=2*(gradxdot_train.T@P@x_train2+P@xdot_train2)
+
     V2=(x_train2.T@P@x_train2).diagonal()
     Vdot2=(xdot_train2.T@P@x_train2+x_train2.T@P@xdot_train2).diagonal()
-    print("Vdot2: [" + ", ".join(f"{v:10.3f}" for v in Vdot2.tolist()) + "]")
-    plt.plot(x_train2[1,:],Vdot2,'o',color=(0.2+0.8*i/2,0,0))
+    Vdot2_res=torch.relu(Vdot2)
+    Vdot2_sign=torch.sign(Vdot2_res)
+    Vdot2_count=Vdot2_res.sum().item()
 
+    # maximize zeros after ReLU, try the boundary
+    x_train2=x_train2+0.001*gradVdot*(1-Vdot2_sign)*-1
+    # minimize already positive after ReLU, try the boundary
+    x_train2=x_train2-0.001*gradVdot*(Vdot2_sign)*-1
+
+    costvec[i]=Vdot2_count
+    costindex[i]=i
+
+plt.figure()
+plt.plot(costindex,costvec,'b')
 plt.show()
+
